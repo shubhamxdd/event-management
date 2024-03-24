cd38246c10c479d8f39d8a2809697d5cb2bf0a40
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-24 20:32:52 +0530

    create user and event schema

---
+++ lib/db/models/userModel.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
