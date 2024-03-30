745d92278e5d3327f58d9263112b7ac9b985237a
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-30 14:18:18 +0530

    customize loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,9 @@ import Spinner from "@/components/Spinner";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Spinner /> Loading
+    <div className="flex flex-row items-center justify-center h-screen gap-3">
+      <Spinner />
+      <h3 className="text-xl font-semibold">Loading</h3>
     </div>
   );
 };
