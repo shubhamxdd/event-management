791ad5f2e992eaa8849fae27c7f600cfb03bab61
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-03-30 14:14:43 +0530

    add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/components/Spinner";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Spinner /> Loading
+    </div>
+  );
+};
+
+export default Loading;
